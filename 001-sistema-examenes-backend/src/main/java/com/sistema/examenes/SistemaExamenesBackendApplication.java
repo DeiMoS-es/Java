6452b392207c2babc869b39ce5c2bf22e2003173
DeiMoS <ndelgado@hiberus.com>
2023-05-24 17:30:57 +0200

    creado usuario prueba

--- 001-sistema-examenes-backend/src/main/java/com/sistema/examenes/SistemaExamenesBackendApplication.java
+++ 001-sistema-examenes-backend/src/main/java/com/sistema/examenes/SistemaExamenesBackendApplication.java
@@ -1,13 +1,48 @@
 package com.sistema.examenes;
 
+import com.sistema.examenes.entity.Rol;
+import com.sistema.examenes.entity.Usuario;
+import com.sistema.examenes.entity.UsuarioRol;
+import com.sistema.examenes.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SistemaExamenesBackendApplication {
+import java.util.HashSet;
+import java.util.Set;
 
+@SpringBootApplication
+public class SistemaExamenesBackendApplication implements CommandLineRunner {
+
+	@Autowired
+	private UsuarioService usuarioService;
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaExamenesBackendApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Nagib");
+		usuario.setApellido("Delgado");
+		usuario.setUsername("Deimos");
+		usuario.setPassword("1234");
+		usuario.setEmail("nagib.86@gmail.com");
+		usuario.setTelefono("123456789");
+		usuario.setPerfil("foto.png");
+
+		Rol role = new Rol();
+		role.setRolId(1L);
+		role.setNombre("ADMIN");
+
+		Set<UsuarioRol> usuarioRoles = new HashSet<>();
+		UsuarioRol usuarioRol = new UsuarioRol();
+		usuarioRol.setRol(role);
+		usuarioRol.setUsuario(usuario);
+		usuarioRoles.add(usuarioRol);
+
+		Usuario usuarioGuardado = usuarioService.guardarUsuario(usuario, usuarioRoles);
+		System.out.println(usuarioGuardado.getUsername());
+	}
 }
