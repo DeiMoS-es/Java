1ca9c5859fc78067aa983feb3b3b251c8ca3011f
DeiMoS <ndelgado@hiberus.com>
2023-06-06 10:04:52 +0200

    creados repositorios usuario y rol

---
+++ 005-spring-security-jwt/src/main/java/com/example/jwt/spring3/repository/RolRepository.java
@@ -0,0 +1,9 @@
+package com.example.jwt.spring3.repository;
+
+import com.example.jwt.spring3.entity.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RolRepository extends JpaRepository<Rol, Integer> {
+}
---
+++ 005-spring-security-jwt/src/main/java/com/example/jwt/spring3/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package com.example.jwt.spring3.repository;
+
+import com.example.jwt.spring3.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository  extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findOneByEmail(String email);
+    Optional<Usuario> findByUserName(String userName);
+}
