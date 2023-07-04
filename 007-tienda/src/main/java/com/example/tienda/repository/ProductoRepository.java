433f2b5feaed5d354364115e45028620fe02c32b
DeiMoS <ndelgado@hiberus.com>
2023-07-04 17:05:12 +0200

    creado repositorios de producto y pedido

--- 007-tienda/src/main/java/com/example/tienda/repository/ProductoRepository.java
+++ 007-tienda/src/main/java/com/example/tienda/repository/ProductoRepository.java
@@ -1,4 +1,12 @@
 package com.example.tienda.repository;
 
-public interface ProductoRepository {
+import com.example.tienda.entity.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+    Optional<Producto> findByNombreProducto(String nombreProducto);
 }
