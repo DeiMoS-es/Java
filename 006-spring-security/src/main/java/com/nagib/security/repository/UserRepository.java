c84cb70404f7ece72fb76ccb11b9068475ff44be
DeiMoS <ndelgado@hiberus.com>
2023-07-04 10:18:31 +0200

    configurado userdetails y usuarioRepository

---
+++ 006-spring-security/src/main/java/com/nagib/security/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.nagib.security.repository;
+
+import com.nagib.security.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
---
+++ 006-spring-security/src/main/java/com/nagib/security/user/Role.java
@@ -0,0 +1,6 @@
+package com.nagib.security.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
