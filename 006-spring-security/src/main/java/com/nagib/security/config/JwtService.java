318a4cb73b74366cf9a99ad89847da30b8b2f754
DeiMoS <ndelgado@hiberus.com>
2023-07-04 11:44:41 +0200

    extraer un claim

--- 006-spring-security/src/main/java/com/nagib/security/config/JwtService.java
+++ 006-spring-security/src/main/java/com/nagib/security/config/JwtService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import io.jsonwebtoken.Claims;
 
 import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -15,6 +16,11 @@ public class JwtService {
     public String extractUsername(String token) {
 
         return null;
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
     }
 
     //Método para extraer todos los Claims(reclamos)
