1d121476633e0f0a5c5663de9dfd380223b12860
DeiMoS <ndelgado@hiberus.com>
2023-07-04 17:00:45 +0200

    creada entidad pedido

--- 007-tienda/src/main/java/com/example/tienda/entity/Pedido.java
+++ 007-tienda/src/main/java/com/example/tienda/entity/Pedido.java
@@ -1,4 +1,30 @@
 package com.example.tienda.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPedido;
+
+    private LocalDate fechaPedido;
+
+    @NotNull
+    private String estado;
+
+    @OneToMany(mappedBy = "pedido", fetch = FetchType.LAZY)
+    private List<Producto> productos;
+
 }
