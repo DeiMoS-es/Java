f16c3fb57a48403cd68079558bbea4b4fae60210
DeiMoS <ndelgado@hiberus.com>
2023-07-04 11:41:42 +0200

    codificación y decodificación de contraseña

--- 006-spring-security/src/main/java/com/nagib/security/config/JwtService.java
+++ 006-spring-security/src/main/java/com/nagib/security/config/JwtService.java
@@ -1,11 +1,17 @@
 package com.nagib.security.config;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 import io.jsonwebtoken.Claims;
 
+import java.security.Key;
+
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
     public String extractUsername(String token) {
 
         return null;
@@ -19,6 +25,11 @@ public class JwtService {
                 .build()
                 .parseClaimsJwt(token)
                 .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 
 }
