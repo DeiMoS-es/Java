1e2579cc25e8860603db6182aeaaf5c1d067b36a
DeiMoS <ndelgado@hiberus.com>
2023-09-08 14:43:52 +0200

    creado Dto

--- 010-apiRest/src/main/java/com/apiRest/producto/dto/ProductoDTO.java
+++ 010-apiRest/src/main/java/com/apiRest/producto/dto/ProductoDTO.java
@@ -1,4 +1,15 @@
 package com.apiRest.producto.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductoDTO {
+
+    private String nombreProducto;
+    private Double precioProducto;
+    private Double ivaProducto;
 }
