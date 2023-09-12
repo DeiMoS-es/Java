622ce9724ab0cba1f983e60aa5779de1d08c2482
DeiMoS <ndelgado@hiberus.com>
2023-09-12 13:37:26 +0200

    creado dto para pedido

--- 010-apiRest/src/main/java/com/apiRest/pedido/dto/PedidoDTO.java
+++ 010-apiRest/src/main/java/com/apiRest/pedido/dto/PedidoDTO.java
@@ -1,4 +1,28 @@
 package com.apiRest.pedido.dto;
 
+import com.apiRest.pedido.entity.Pedido;
+import com.apiRest.producto.entity.Producto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PedidoDTO {
+    //quiero ver la fecha del pedido, el estado, los productos y el //TODO usuario que lo ha hecho
+    private LocalDateTime fechaPedido;
+    private String estado;
+    private List<Producto> productos;
+
+    public static PedidoDTO fromPedido(Pedido pedido){
+        PedidoDTO dto = new PedidoDTO();
+        dto.setFechaPedido(pedido.getFechaPedido());
+        dto.setEstado(pedido.getEstado());
+        dto.setProductos(pedido.getProductos());
+        return dto;
+    }
 }
