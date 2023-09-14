43939202a3e86cb0bdb079af62e959b38b39488d
DeiMoS <ndelgado@hiberus.com>
2023-09-14 09:30:13 +0200

    funcionalidad ver producto y template

--- 011-spring-ecommerce/src/main/java/com/ecommerce/controller/HomeController.java
+++ 011-spring-ecommerce/src/main/java/com/ecommerce/controller/HomeController.java
@@ -3,9 +3,12 @@ package com.ecommerce.controller;
 
 import com.ecommerce.service.ProductoService;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,9 +17,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     private final ProductoService productoService;
+    private final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
     @GetMapping
     public String home(Model model){
         model.addAttribute("productos", productoService.findAll());
         return "usuario/home";
+    }
+
+    @GetMapping("productohome/{idProducto}")
+    public String productoHome(@PathVariable("idProducto") Integer idProducto){
+        LOGGER.info("Id producto enviado como parámetro {}", idProducto);
+        return "usuario/productohome";
     }
 }
