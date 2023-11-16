8a44b8b67b9b9f6f0d37c406caa5259adc9d48af
DeiMoS <ndelgado@hiberus.com>
2023-11-16 12:13:49 +0100

    manejo de excepcion su las credenciales no corresponden, AuthController

--- 012-spring-autentication-JWT/src/main/java/com/example/auth/AuthController.java
+++ 012-spring-autentication-JWT/src/main/java/com/example/auth/AuthController.java
@@ -1,6 +1,7 @@
 package com.example.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,7 +14,14 @@ public class AuthController {
     private final AuthService authService;
     @PostMapping(value = "login")
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request){
-        return ResponseEntity.ok(authService.login(request));
+        try {
+            AuthResponse authResponse = authService.login(request);
+            return ResponseEntity.ok(authResponse);
+        } catch (Exception e) {
+            // Log de la excepción si ocurre un error durante el inicio de sesión
+            System.out.println("Error durante el inicio de sesión: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
     }
 
     @PostMapping(value = "register")
