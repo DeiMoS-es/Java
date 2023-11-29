fd5fbb9354d5e947af93aa62999b4a2be60f2b21
DeiMoS <ndelgado@hiberus.com>
2023-11-29 17:32:57 +0100

    impedir archivos que no sean imagenes

--- 013-spring-apirest/src/main/java/com/tiendaProductos/cloudinary/controller/CloudinaryController.java
+++ 013-spring-apirest/src/main/java/com/tiendaProductos/cloudinary/controller/CloudinaryController.java
@@ -1,5 +1,6 @@
 package com.tiendaProductos.cloudinary.controller;
 
+import com.tiendaProductos.cloudinary.dto.Mensaje;
 import com.tiendaProductos.cloudinary.service.CloudinaryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -7,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Map;
 
@@ -17,13 +20,17 @@ public class CloudinaryController {
 
     @Autowired
     CloudinaryService cloudinaryService;
-    @PostMapping("/uload")
+    @PostMapping("/upload")
     public ResponseEntity<Map> upload(@RequestParam MultipartFile multipartFile) throws IOException {
+        BufferedImage bi = ImageIO.read(multipartFile.getInputStream());
+        if(bi==null){
+            return new ResponseEntity(new Mensaje("Imagen no valida."), HttpStatus.BAD_REQUEST);
+        }
         Map result = cloudinaryService.upload(multipartFile);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    @DeleteMapping("/delete/{idImagen")
+    @DeleteMapping("/delete/{idImagen}")
     public ResponseEntity<Map> upload(@PathVariable("idImagen") String idImagen) throws IOException {
         Map result = cloudinaryService.delete(idImagen);
         return new ResponseEntity<>(result, HttpStatus.OK);
