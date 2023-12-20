b112cd4d4e768f1ca77ec2891062d849cfb85498
DeiMoS <ndelgado@hiberus.com>
2023-12-20 17:03:43 +0100

    implement UserDetails

--- 013-spring-apirest/src/main/java/com/tiendaProductos/user/entity/User.java
+++ 013-spring-apirest/src/main/java/com/tiendaProductos/user/entity/User.java
@@ -5,22 +5,55 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Table(name = "user", uniqueConstraints = {@UniqueConstraint(columnNames = {"userName"})}) // Indicamos que la columna userName no puede repetirse el valor
-public class User {
+@Table(name = "user", uniqueConstraints = {@UniqueConstraint(columnNames = {"username"})}) // Indicamos que la columna userName no puede repetirse el valor
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idUsuario;
     @Column(nullable = false) // no permite insert de valores null
-    private String userName;
+    private String username;
     private String password;
     private String nombre;
     private String apellidos;
     private String pais;
-    Role rol;
+    @Enumerated(EnumType.STRING)
+    Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    // Los que devuelven boolean los ponemos a TRUE, ya que como estamos trabajando con tokens, en el token vendrán todos esos datos
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
