883eec6a491a26f0e3b886a904c997fc7a81a563
DeiMoS <ndelgado@hiberus.com>
2023-12-20 12:40:48 +0100

    configurado JwtAuthenticationFilter

--- 013-spring-apirest/src/main/java/com/tiendaProductos/security/jwt/JwtAuthenticationFilter.java
+++ 013-spring-apirest/src/main/java/com/tiendaProductos/security/jwt/JwtAuthenticationFilter.java
@@ -1,4 +1,38 @@
 package com.tiendaProductos.security.jwt;
 
-public class JwtAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import org.springframework.http.HttpHeaders;
+
+@Component
+@AllArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        //Obtenemos el token del request
+        final String token = getToenFromRequest(request);
+        if(token == null){
+            filterChain.doFilter(request, response);//devolvemos el control a la cadena de filtros
+            return;
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String getToenFromRequest(HttpServletRequest request) {
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION); // Obtengo los encabezados
+        if(StringUtils.hasText(authHeader) && authHeader.startsWith("Bearer ")){
+            return authHeader.substring(7);
+        }
+        return null;
+    }
 }
