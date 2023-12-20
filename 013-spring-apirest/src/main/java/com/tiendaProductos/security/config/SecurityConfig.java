2331c73ed7c483427ff19accbe59d3b6113a729e
DeiMoS <ndelgado@hiberus.com>
2023-12-20 12:09:47 +0100

    primera versión de securityFilterChain

--- 013-spring-apirest/src/main/java/com/tiendaProductos/security/config/SecurityConfig.java
+++ 013-spring-apirest/src/main/java/com/tiendaProductos/security/config/SecurityConfig.java
@@ -1,4 +1,31 @@
 package com.tiendaProductos.security.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/*
+* Esta clase contendrá la cadena de filtros que usa jwt
+*/
+@Configuration
+@EnableWebSecurity
+
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authRequest ->
+                        authRequest
+                                .requestMatchers("/auth/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
 }
--- 013-spring-apirest/src/main/java/com/tiendaProductos/security/controller/AuthController.java
+++ 013-spring-apirest/src/main/java/com/tiendaProductos/security/controller/AuthController.java
@@ -1,4 +1,20 @@
 package com.tiendaProductos.security.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin
+@RequiredArgsConstructor
 public class AuthController {
+
+    @PostMapping("/login")
+    public String login(){
+        return "Login from public endpoint";
+    }
+    @GetMapping("/register")
+    public String register(){
+        return "Register from public endpoint";
+    }
 }
