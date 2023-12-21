779f3963879a575641eef43201f4ec712113694d
DeiMoS <ndelgado@hiberus.com>
2023-12-21 17:21:02 +0100

    configurado métodos para obtener el token

--- 013-spring-apirest/src/main/java/com/tiendaProductos/security/service/AuthService.java
+++ 013-spring-apirest/src/main/java/com/tiendaProductos/security/service/AuthService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final JwtService jwtService;
     public AuthResponse login(LoginRequest request) {
         return null;
     }
@@ -29,7 +30,7 @@ public class AuthService {
                 .build();
         userRepository.save(user);
         return AuthResponse.builder()
-                .token(null)
+                .token(jwtService.getToken(user))
                 .build();
     }
 }
