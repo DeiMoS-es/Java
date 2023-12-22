33b86e2c1d36419570448f308f6997f1eee37138
DeiMoS <ndelgado@hiberus.com>
2023-12-22 11:37:39 +0100

    actualizado el filtro de autenticación de JwtAuthenticationFilter

--- 013-spring-apirest/src/main/java/com/tiendaProductos/security/jwt/JwtAuthenticationFilter.java
+++ 013-spring-apirest/src/main/java/com/tiendaProductos/security/jwt/JwtAuthenticationFilter.java
@@ -1,10 +1,16 @@
 package com.tiendaProductos.security.jwt;
 
+import com.tiendaProductos.security.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -15,15 +21,31 @@ import org.springframework.http.HttpHeaders;
 @Component
 @AllArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
         //Obtenemos el token del request
         final String token = getToenFromRequest(request);
+        final String username;
         if(token == null){
             filterChain.doFilter(request, response);//devolvemos el control a la cadena de filtros
             return;
+        }
+        username = jwtService.getUsernameFromToken(token);
+        //Si el username es distinto de null y no se encuentra en el SecurityContextHolder, lo buscamos en BBDD
+        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            //Validamos si el token es válido
+            if(jwtService.isTokenValid(token, userDetails)){
+                //Actualizo el SecurityContextHolder, creando un UsernamePasswordAuthenticationToken
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
         }
         filterChain.doFilter(request, response);
     }
