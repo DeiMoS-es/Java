a8943062ea541508bf751aee793dd478753d0c5e
DeiMoS <ndelgado@hiberus.com>
2023-12-22 14:56:50 +0100

    actualización libreria Jwt

--- 013-spring-apirest/src/main/java/com/tiendaProductos/security/service/JwtService.java
+++ 013-spring-apirest/src/main/java/com/tiendaProductos/security/service/JwtService.java
@@ -1,12 +1,14 @@
 package com.tiendaProductos.security.service;
 
+import com.tiendaProductos.user.entity.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
 import java.util.function.Function;
 
 import java.security.Key;
@@ -14,21 +16,24 @@ import java.util.*;
 @Service
 public class JwtService {
     private static final String SECRET_KEY="586E3272357538782F413F4428472B4B6250655368566B597033733676397924";
-    public String getToken(UserDetails user) {
+    public String getToken(User user) {
         return getToken(new HashMap<>(), user);
     }
 
-    private String getToken(Map<String, Objects> extraClaims, UserDetails user) {
+    private String getToken(Map<String, Objects> extraClaims, User user) {
         return Jwts.builder()
-                .setClaims(extraClaims)
-                .setSubject(user.getUsername())
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis()+ 1000 * 60 * 60 * 24))
-                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .claims(extraClaims)
+                .claim("nombre", user.getIdUsuario())
+                .claim("nombre", user.getNombre())
+                .claim("apellidos", user.getApellidos())
+                .subject(user.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis()+ 1000 * 60 * 60 * 24))
+                .signWith(getKey())
                 .compact();
     }
 
-    private Key getKey() {
+    private SecretKey getKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
     }
@@ -45,11 +50,11 @@ public class JwtService {
 
     private Claims getAllClaims(String token){
         return Jwts
-                .parserBuilder()
-                .setSigningKey(getKey())
+                .parser()
+                .verifyWith(getKey())
                 .build()
-                .parseClaimsJws(token)
-                .getBody();
+                .parseSignedClaims(token)
+                .getPayload();// Obtenemos el payload, que es una de las partes que formas el Jwt, concretamente donde vienen los datos que queramos añadir el Jwt
     }
 
     //Método para obtener un Claim en particular
