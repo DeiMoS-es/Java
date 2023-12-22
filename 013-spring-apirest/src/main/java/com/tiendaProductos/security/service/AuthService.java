e9b329269b9a8830bcef59be88346667854f1ef4
DeiMoS <ndelgado@hiberus.com>
2023-12-22 11:25:16 +0100

    configurado login de usuario

--- 013-spring-apirest/src/main/java/com/tiendaProductos/security/service/AuthService.java
+++ 013-spring-apirest/src/main/java/com/tiendaProductos/security/service/AuthService.java
@@ -7,6 +7,9 @@ import com.tiendaProductos.user.entity.Role;
 import com.tiendaProductos.user.entity.User;
 import com.tiendaProductos.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -17,9 +20,15 @@ public class AuthService {
     private final UserRepository userRepository;
     private final JwtService jwtService;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
     public AuthResponse login(LoginRequest request) {
-        return null;
-    }
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        UserDetails user = userRepository.findByUsername(request.getUsername()).orElseThrow();
+        String token = jwtService.getToken(user);
+        return AuthResponse.builder()
+                .token(token)
+                .build();
+    }//En la respuesta enviamos el token
 
     public AuthResponse register(RegisterRequest request) {
         User user = User.builder()
